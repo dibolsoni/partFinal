b371354baaad2020ad5cce1ae90e6c9a7dba2055
dibolsoni <dibolsoni@gmail.com>
2019-07-13 20:12:11 -0300

    header and courses done

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,30 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+
+import Header from './components/Header';
+import Courses from './components/Courses'
+
+import withContext from './Context';
+const HeaderWithContext = withContext(Header);
+const CoursesWithContext = withContext(Courses);
 
 
 
-function App() {
-  async function fetchData(url) {
-    const method = 'GET';
-    const options = {
-      method,
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-    }
-    const response = await fetch(url, options)
-    const json = await response.json();
-    const courses = await json.courses;
+export default() => (
+  <Router>
+    <div>
+      <HeaderWithContext />
 
-    return courses    
-  }
-
-
-  const courses = fetchData('http://localhost:5000/api/courses');
-  console.log('courses', courses)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <div> 
-      {/* { courses} */}
-      </div>
+      <Switch>
+        <Route exact path="/" component={CoursesWithContext} /> 
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
 
-export default App;
+
